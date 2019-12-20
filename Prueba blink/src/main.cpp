d799a5a973d74f91c5e2e08bfeb510bfcc78cafa
Javier Burgos <javierburgosblancas@gmail.com>
2019-12-20 23:50:48 +0100

    update de main

--- Prueba blink/src/main.cpp
+++ Prueba blink/src/main.cpp
@@ -2,10 +2,14 @@
 
 void setup() {
   Serial.begin(9600);
+  pinMode(13,OUTPUT);
 
 }
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("Hello from the other side");
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
   delay(1000);
 }
